9841f35ca23b355903851457caa7e0b551ab59b9
Huy le <lexuanhuy8695@gmail.com>
2025-09-28 20:52:22 +0700

    fix

--- apps/web/src/components/layout/Header/components/NotificationItem.tsx
+++ apps/web/src/components/layout/Header/components/NotificationItem.tsx
@@ -66,11 +66,11 @@ const NotificationItem: React.FC<NotificationItemProps> = ({
     let targetUrl: string;
     if (isProduction) {
       // Production: use env variable
-      const baseUrl = "http://localhost:3000";
+      const baseUrl = "https://hanotex.vn";
       targetUrl = `${baseUrl}/${cleanActionUrl}`;
     } else {
       // Development: use env variable
-      const baseUrl = "http://localhost:3000";
+      const baseUrl = "https://hanotex.vn";
       targetUrl = `${baseUrl}/${cleanActionUrl}`;
     }
 
